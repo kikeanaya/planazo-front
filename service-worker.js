236ad39fe07d4b2651cf93b68aea0cb04dbef82a
kikeanaya <luiseanayam@gmail.com>
2019-05-14 13:57:15 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/planazo-front/precache-manifest.d5653cbd90a9950a31c150be676ace63.js"
+  "/planazo-front/precache-manifest.844741d82abd12d0c396ae48ae5506d8.js"
 );
 
 workbox.clientsClaim();
